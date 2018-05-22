eb503987bbe93a22868443fdf22e2d074260f400
leiteigors <leiteigors@gmail.com>
2018-05-22 19:28:07 -0300

    V 1.0

---
+++ SQLQueryProcedures.sql
@@ -0,0 +1,52 @@
+﻿--create procedure ExcluirPorId
+--(
+--@timeId int
+--)
+--as
+
+--begin
+--Delete From [dbo].[Table] where TimeId = @timeId
+--end
+
+create procedure ObterPorId
+(
+@timeId int
+)
+as
+
+begin
+Select * From [dbo].[Table] where TimeId = @timeId
+end
+
+
+
+--create procedure AtualizarTime
+--(
+--@timeId int,
+--@Time nvarchar(50),
+--@Estado char(2),
+--@Cores varchar(50)
+--)
+
+--as
+
+--begin
+--Update [dbo].[Table] set Time = @Time, Estado = @Estado, Cores = @Cores where TimeId = @timeId
+--end
+
+--create procedure InserirTime
+--(
+--@Time nvarchar(50),
+--@Estado char(2),
+--@Cores varchar(50)
+--)
+--as
+--begin
+--Insert into [dbo].[Table] values (@Time, @Estado, @Cores) 
+--end
+
+create procedure ObterTimes
+as
+begin
+Select * from [dbo].[Table] 
+end
